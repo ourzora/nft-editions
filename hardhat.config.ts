6c23157f8b82c8f47af8d4966b19540b877ec395
Zoe Nolan <github@zoenolan.org>
2022-06-22 11:51:56 +0100

    Update packages

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,26 +4,61 @@ import "hardhat-gas-reporter";
 import "hardhat-deploy";
 import "@nomiclabs/hardhat-etherscan";
 import { HardhatUserConfig } from "hardhat/config";
-import networks from "./networks";
-import dotenv from "dotenv";
-import "solidity-coverage";
+import dotenv from 'dotenv';
 
 dotenv.config();
+
+const rinkebyBaseUrl = process.env.RINKEBY_URL || "";
+const goerliBaseUrl = process.env.GOERLI_URL || "";
+const mainnetBaseUrl = process.env.MAINNET_URL || "";
+
+const apiKey = process.env.INFURA_API_KEY || "";
+
+const rinkebyUrl = rinkebyBaseUrl.concat(apiKey);
+const goerliUrl = goerliBaseUrl.concat(apiKey);
+const mainnetUrl = mainnetBaseUrl.concat(apiKey);
 
 /**
  * Go to https://hardhat.org/config/ to learn more
  * @type import('hardhat/config').HardhatUserConfig
  */
 const config: HardhatUserConfig = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: 'hardhat',
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
   gasReporter: {
-    currency: "USD",
-    gasPrice: 60,
+    currency: 'USD',
+    gasPrice: 60, 
   },
-  networks,
+  networks: {
+    hardhat: {
+      forking: {
+        url: mainnetUrl,
+      },
+    },
+    rinkeby: {
+      url: rinkebyUrl,
+      accounts:
+        process.env.TREASURY_PRIVATE_KEY !== undefined
+          ? [process.env.TREASURY_PRIVATE_KEY]
+          : [],
+    },
+    goerli: {
+      url: goerliUrl,
+      accounts:
+        process.env.TREASURY_PRIVATE_KEY !== undefined
+          ? [process.env.TREASURY_PRIVATE_KEY]
+          : [],
+    },
+    mainnet: {
+      url: mainnetUrl,
+      accounts:
+        process.env.TREASURY_PRIVATE_KEY !== undefined
+          ? [process.env.TREASURY_PRIVATE_KEY]
+          : [],
+    },
+  },
   namedAccounts: {
     deployer: 0,
     purchaser: 0,
